b431d35edf164383c1d78a80d9ae7c397f6ba796
Rick Vermeil <rickv85@gmail.com>
2023-02-21 10:20:37 -0700

    Add get call

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+function App() {
+  const [allReservations, setAllReservations] = useState([]);
+  const [error, setError] = useState('');
 
-        </div>
-        <div className='resy-container'>
-          
-        </div>
-      </div>
-    )
+  const getAllReservations = async () => {
+    const url = 'http://localhost:3001/api/v1/reservations';
+  
+    try {
+      const response = await fetch(url);
+      const reservations = await response.json();
+      setAllReservations(reservations);
+    } catch (error) {
+      setError(error.message);
+    }
   }
+
+   useEffect(() => {
+    getAllReservations()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1 className='app-title'>Turing Cafe Reservations</h1>
+      <div className='resy-form'>
+
+      </div>
+      <div className='resy-container'>
+        
+      </div>
+    </div>
+  )
+
 }
 
 export default App;
